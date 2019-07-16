a89b638ba360cdad0f4f5c30171a792b6cab30fb
Marcelo Esser <esser@gs07.local>
2019-07-16 15:28:53 -0300

    lottie implemented

--- app/src/main/java/marcelo/esser/com/bustimek/vvm/home/HomeActivity.kt
+++ app/src/main/java/marcelo/esser/com/bustimek/vvm/home/HomeActivity.kt
@@ -1,15 +1,14 @@
 package marcelo.esser.com.bustimek.vvm.home
 
-import android.content.Intent
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_home.*
 import marcelo.esser.com.bustimek.R
-import marcelo.esser.com.bustimek.vvm.sogal.lines.SogalLinesActivity
-import marcelo.esser.com.bustimek.vvm.vicasa.lines.VicasaLinesActivity
 
 class HomeActivity : AppCompatActivity() {
+
+var control = false
 
     private val activityContext by lazy {
         this@HomeActivity
@@ -19,12 +18,23 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        cv_home_activity_vicasa.setOnClickListener {
+        /*cv_home_activity_vicasa.setOnClickListener {
             startActivity(Intent(activityContext, VicasaLinesActivity::class.java))
         }
 
         cv_home_activity_sogal.setOnClickListener {
             startActivity(Intent(activityContext, SogalLinesActivity::class.java))
+        }*/
+
+        animation_view.setOnClickListener {
+            control = !control
+            val animator = if (control) ValueAnimator.ofFloat(0f, 0.5f) else ValueAnimator.ofFloat(0.5f, 1f)
+            animator.addUpdateListener { animation ->
+                animation_view.progress = animation.animatedValue as Float
+            }
+            animator.duration =700
+            animator.start()
         }
     }
+
 }
